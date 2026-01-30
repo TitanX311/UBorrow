ccd45b7b69416a9d008da66cb49463b18ec9ac45
TitanX311 <abirdebnath311@hotmail.com>
2026-01-30 23:35:57 +0530

    ui updates

---
+++ lib/core/repository/cloudinary_provider.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cloudinary_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$cloudinaryConfigHash() => r'69c477510a7f6e25a1c37e25432f8a71f0d4e5c4';
+
+/// Cloudinary configuration provider
+///
+/// Copied from [cloudinaryConfig].
+@ProviderFor(cloudinaryConfig)
+final cloudinaryConfigProvider = AutoDisposeProvider<CloudinaryConfig>.internal(
+  cloudinaryConfig,
+  name: r'cloudinaryConfigProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$cloudinaryConfigHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CloudinaryConfigRef = AutoDisposeProviderRef<CloudinaryConfig>;
+String _$cloudinaryServiceHash() => r'85b0aff1ced6326572c7a7348ce2fcc7250e2516';
+
+/// Cloudinary service provider
+///
+/// Copied from [cloudinaryService].
+@ProviderFor(cloudinaryService)
+final cloudinaryServiceProvider =
+    AutoDisposeProvider<CloudinaryService>.internal(
+      cloudinaryService,
+      name: r'cloudinaryServiceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$cloudinaryServiceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CloudinaryServiceRef = AutoDisposeProviderRef<CloudinaryService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
